4c476cad940ba42ffa0708dd0172f4700abd502b
Matt Kelly <matthew.kelly2@gmail.com>
2014-08-05 12:02:52 -0400

    removed duplicate files

--- package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains several classes that are useful for simulating robots.
- */
-/**
- * @author Matthew Kelly, Cornell Univerisity, May 23, 2014
- *
- */
-package mpk_gui;
