6e018a050a46643101c28231308f47b488bd6f6e
Matt Kelly <matthew.kelly2@gmail.com>
2014-08-05 11:16:43 -0400

    Initial commit of pre-existing files. Mostly for simple plotting and simulation examples.

---
+++ package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains several classes that are useful for simulating robots.
+ */
+/**
+ * @author Matthew Kelly, Cornell Univerisity, May 23, 2014
+ *
+ */
+package mpk_gui;
